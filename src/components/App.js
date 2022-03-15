c0bf3bb7529d2e9c3277c3359f277720350607a6
yuu30-dev <y.nagai0330@gmail.com>
2022-03-15 09:27:25 +0900

    save name in japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 
+import Main from './Main';
 import SignIn from './SignIn';
 
 export default () => {
   const [name, setName] = useState('');
   console.log({ name });
 
-  return <SignIn setName={setName} />;
+  if (name === '') {
+    return <SignIn setName={setName} />;
+  }
+  return <Main name={name} />;
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,8 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
-  console.log({ disabled, string });
+  const [isComposed, setIsComposed] = useState(false);
+  console.log({ disabled, string, isComposed });
 
   useEffect(() => {
     setDisabled(string === '');
@@ -73,11 +74,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
+
               if (e.key === 'Enter') {
                 e.preventDefault();
                 setName(e.target.value);
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
