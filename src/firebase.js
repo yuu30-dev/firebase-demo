a8ccd9d55d3748bf4d4169f4f8c8c32e49975c98
yuu30-dev <y.nagai0330@gmail.com>
2022-03-18 07:39:25 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push } from 'firebase/database';
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyAhS1nSiRFFKV3ejoDf9jS7Urc--q73brA',
-  authDomain: 'fir-demo-5a707.firebaseapp.com',
-  databaseURL: 'https://fir-demo-5a707-default-rtdb.firebaseio.com',
-  projectId: 'fir-demo-5a707',
-  storageBucket: 'fir-demo-5a707.appspot.com',
-  messagingSenderId: '90656263543',
-  appId: '1:90656263543:web:93ff08e8fb9545ee5da572',
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
